90e0a441197efba5a4b72d4156f8a0bc4d4e0d1f
Jakub Kozisek <jakub.kozisek@gmail.com>
2011-11-26 02:58:10 +0100

    forgot to remove temporary miniserver

--- examples/modular-backbone/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-    app    = express.createServer(),
-    port   = process.env.PORT || 3000;
-
-app.use(express.static(__dirname));
-
-app.listen(port, function () {
-    console.log('Server listening on ' + port);
-});
